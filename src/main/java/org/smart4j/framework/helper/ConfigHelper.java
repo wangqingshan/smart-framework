7c14a2fb2febf26c989f43dce08a21af7b550f04
wangqingshan <1287265008@qq.com>
2019-06-09 23:52:11 +0800

    提交初始文件

---
+++ src/main/java/org/smart4j/framework/helper/ConfigHelper.java
@@ -0,0 +1,15 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.ConfigConstant;
+
+import java.util.Properties;
+
+/**
+ * author 90
+ * description 属性文件助手类
+ * date 2019/6/9
+ */
+public class ConfigHelper {
+
+    //private static final Properties CONFIG_PROPS=PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+}
